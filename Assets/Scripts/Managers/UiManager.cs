6f2404890c0a9d1c226c2c48faa953b178d4195d
nikitaartamonov <nikitaartamonov@mail.ru>
2023-05-11 17:40:37 +0300

    added update current points method

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -7,11 +7,17 @@ namespace Managers
     public class UiManager : MonoBehaviour
     {
         [SerializeField] private TMP_Text currentPrice;
+        [SerializeField] private TMP_Text currentPoints;
         public ISkillButtonView SelectedButton;
 
         public void UpdateCurrentPrice(int price)
         {
             currentPrice.text = $"Current Price: {price}";
+        }
+
+        public void UpdateCurrentPoints(int points)
+        {
+            currentPoints.text = $"{points} Points";
         }
     }
 }
